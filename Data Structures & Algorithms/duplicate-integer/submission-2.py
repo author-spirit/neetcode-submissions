6feed801acadf407303f6744f01846a83bfaefb2
Harry <45410122+author-spirit@users.noreply.github.com>
2026-05-27 07:03:27 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # store in one medium to get the frequency counts
+        # we can have an array exlcuding the 0th index, and use rest
+        # this is exist to resolve the repeat numbers, 
+        # one catch is it can also accept neg've numbers
+        # so this looks like frequency count but twist is neg-ve
+        # sort and keep previous number
+        # nums.sort()
+        # for i in range(1,len(nums)):
+        #     if nums[i] == nums[i-1]:
+        #         return True
+        
+        # return False
+
+        repeat = float('inf')
+        nums.sort()
+        for i in nums:
+            if repeat == i:
+                return True
+            else:
+                repeat = i
+        
+        return False
+        
